2715b927ac8c8dc843508249e099edd80149c26e
Navaneeth Krishna <krishnanavaneeth551@gmail.com>
2024-07-26 21:31:05 +0530

    added about page

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 console.log("hello world");
-
-let user_name = window.prompt("Hiya, whats you name?(lowercase)");
-/*
-if(user_name == "appu"){
-    console.log("hi appu")
-}
-*/
-
-console.log("hi " + user_name)
